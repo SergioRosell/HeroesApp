a76bba1e88eb56b655f61acec58b2bfc3d4a50f3
Sergio <sergio.rosell.dev@gmail.com>
2023-11-23 10:13:51 +0100

    segundo commit

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { environments } from 'src/environments/environments';
 import { Hero } from '../interfaces/hero.interface';
 
@@ -10,12 +10,24 @@ export class HeroesService {
 
   private baseUrl: string = environments.baseUrl;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   getHeroes():Observable<Hero[]>{
 
-    return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes`);
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
 
+  }
+
+  getHeroById( id: string ): Observable <Hero | undefined>{
+    return this.http.get<Hero>(`${ this.baseUrl}/heroes/${ id }`)
+      .pipe(
+        catchError( error => of(undefined) )
+      );
+  }
+
+
+  getSuggestions( query : string): Observable<Hero[]>{
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${ query }&_limit=6`);
   }
 
 }
